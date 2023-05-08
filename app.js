d4a3fa05f5a5da49985715f17bd0422cf4dc0cfd
Behruz <rakhmatovbekhruz17@gmail.com>
2023-05-08 15:07:47 +0500

    games

---
+++ app.js
@@ -0,0 +1,9 @@
+let menuBtn = document.querySelector(".menu");
+let mobileNav = document.querySelector(".mobile_nav");
+
+menuBtn.addEventListener("click", function () {
+  console.log("Ming sum bering akajonlar");
+    mobileNav.classList.toggle("showNav");
+
+  
+});
